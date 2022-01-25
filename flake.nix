fcac4dfd9cecea0eebb610487ece122bbf596368
David Pätzel <david.paetzel@uni-a.de>
2022-01-25 11:50:49 +0100

    Pin nixpkgs, pin Python version

--- flake.nix
+++ flake.nix
@@ -1,7 +1,9 @@
 {
   description = "pystan";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  # 2022-01-24
+  inputs.nixpkgs.url =
+    "github:NixOS/nixpkgs/8ca77a63599ed951d6a2d244c1d62092776a3fe1";
   inputs.src = {
     url =
       "https://files.pythonhosted.org/packages/10/9f/dcecf31ac08842a675ec337a14439b37d75f6da42f034b1fd5eb90b10b84/pystan-2.19.1.1.tar.gz";
@@ -14,14 +16,15 @@
 
     packages.x86_64-linux.pystan =
       with import nixpkgs { system = "x86_64-linux"; };
-      python3.pkgs.buildPythonPackage {
+      let python = python39;
+      in python.pkgs.buildPythonPackage {
         pname = "pystan";
         version = "2.19.1.1";
         inherit src;
 
         doCheck = false;
 
-        propagatedBuildInputs = with python3.pkgs; [ cython numpy ];
+        propagatedBuildInputs = with python.pkgs; [ cython numpy ];
         meta = with lib; {
           homepage = "https://pypi.org/project/pystan/2.19.1.1/";
           description =
